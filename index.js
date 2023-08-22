1e321170cef3be49594fd8509f86c3476db698bb
Gurman <134552970+GurdevSingh02@users.noreply.github.com>
2023-08-22 08:59:05 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -44,3 +44,22 @@ function checkClicks() {
     }
   }
 }
+
+var restartButton = document.querySelector(".restart-button");
+restartButton.addEventListener("click", restartGame);
+
+function restartGame() {
+  // Reset player clicks and current player
+  clicksPlayer1 = 0;
+  clicksPlayer2 = 0;
+  currentPlayer = 1;
+
+  // Reset dice images
+  var image1 = document.querySelectorAll("img")[0];
+  var image2 = document.querySelectorAll("img")[1];
+  image1.setAttribute("src", "images/dice6.png");
+  image2.setAttribute("src", "images/dice6.png");
+
+  // Reset game result
+  document.querySelector(".container h1").innerHTML = "Dicee Game";
+}
